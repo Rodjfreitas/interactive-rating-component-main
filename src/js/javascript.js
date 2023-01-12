0e1b28165e636aff594a1e9f558b686bd0bf509a
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-12 12:36:44 -0300

    escrevendo código para botão submeter.

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,18 +1,48 @@
 const ratings = document.querySelectorAll('li');
-let ratingSelecionado; 
-console.log(ratings);
+const CardPrincipal = document.querySelector('.Principal-Cartao');
+const CardSecundario = document.querySelector('.Secundario-Cartao');
+const BotaoEnviar = document.querySelector('.Submit-btn');
 
+let ratingSelecionado; 
+
+
+//função que remove a estilização do clique anterior caso mude a opção//
 function SelecionarBotao(e){
   if(ratingSelecionado !== undefined){
     ratings[ratingSelecionado-1].classList.remove('ativo');
   }
 
-
+  //Define qual número foi selecionado e ativa estilização//
   e.target.classList.add('ativo');
   ratingSelecionado = Number(e.target.innerText);
-  console.log(ratingSelecionado);
+  
 }
 
+//Comando do clique//
 ratings.forEach(function(opt){
   opt.addEventListener('click',SelecionarBotao);
 });
+
+
+
+
+
+
+function Enviar(){
+    if(ratingSelecionado !== undefined){
+      CardSecundario.style.display = 'block';
+      CardPrincipal.style.display = 'none';
+         
+    }
+}
+
+
+
+  BotaoEnviar.addEventListener('Click',function());
+ 
+
+
+  
+
+
+
